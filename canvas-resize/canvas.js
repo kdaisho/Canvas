d5e6096e64972b83e9d749d9b439bdb5788e0948
Daisho <daishokomiyama@gmail.com>
2018-11-12 21:37:18 -0500

    Initial commit

---
+++ canvas-resize/canvas.js
@@ -0,0 +1,23 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+c.fillStyle = 'rgba(255, 0, 0, .5)';
+c.fillRect(100, 100, 100, 100);
+c.fillStyle = 'rgba(0, 0, 255, .5)';
+c.fillRect(400, 100, 100, 100);
+c.fillStyle = 'rgba(0, 255, 0, .5)';
+c.fillRect(300, 300, 100, 100);
+
+// Line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = '#fa34a3';
+c.stroke();
+
+console.log(canvas);
