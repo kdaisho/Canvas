30fe0bccf811e46a4ab391aa413a5e56324322c0
Daisho <daishokomiyama@gmail.com>
2018-11-13 22:32:27 -0500

    Adding random 100 circles

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -5,37 +5,72 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-c.fillStyle = 'rgba(255, 0, 0, .5)';
-c.fillRect(100, 100, 100, 100);
-c.fillStyle = 'rgba(0, 0, 255, .5)';
-c.fillRect(400, 100, 100, 100);
-c.fillStyle = 'rgba(0, 255, 0, .5)';
-c.fillRect(300, 300, 100, 100);
+// c.fillStyle = 'rgba(255, 0, 0, .5)';
+// c.fillRect(100, 100, 100, 100);
+// c.fillStyle = 'rgba(0, 0, 255, .5)';
+// c.fillRect(400, 100, 100, 100);
+// c.fillStyle = 'rgba(0, 255, 0, .5)';
+// c.fillRect(300, 300, 100, 100);
 
 // Line
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle = '#fa34a3';
-c.stroke();
-
-// Arc
 // c.beginPath();
-// c.arc(300, 300, 50, 0, Math.PI * 2, false);
-// c.strokeStyle = 'green';
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = '#fa34a3';
 // c.stroke();
 
-// Loop
-for (var i = 0; i < 1000; i++) {
-    var r = Math.random() * 255;
-    var g = Math.random() * 255;
-    var b = Math.random() * 255;
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
-    c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
-    c.strokeStyle = `rgba(${r}, ${g}, ${b}, 1)`;
-    c.stroke();
+function Circle(x, y, dx, dy, radius) {
+    this.x = x || 200;
+    this.y = y || 200;
+    this.dx = dx || 4;
+    this.dy = dy || 4;
+    this.radius = radius || 30;
 
+    this.draw = function() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = 'green';
+        c.stroke();
+        c.fill();
+    }
+
+    this.update = function() {
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
 }
+
+
+// Arc
+var circleArray = [];
+
+for (var i = 0; i < 100; i++) {
+    var radius = 30;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - .5) * 8;
+    var dy = (Math.random() - .5) * 8;
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+console.log(circleArray);
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+
+    for (var i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
+    }
+}
+
+animate();
