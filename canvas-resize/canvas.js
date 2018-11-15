6a328bb69e19611eef35cadc31f49747d1ec4f93
Daisho <daishokomiyama@gmail.com>
2018-11-14 21:29:50 -0500

    Adding mousemove event

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -20,6 +20,16 @@ var c = canvas.getContext('2d');
 // c.strokeStyle = '#fa34a3';
 // c.stroke();
 
+var mouse = {
+    x: undefined,
+    y: undefined
+};
+
+window.addEventListener('mousemove', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
 function Circle(x, y, dx, dy, radius) {
     this.x = x || 200;
     this.y = y || 200;
@@ -30,7 +40,8 @@ function Circle(x, y, dx, dy, radius) {
     this.draw = function() {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = 'green';
+        c.strokeStyle = 'lightgreen';
+        c.lineWidth = 5;
         c.stroke();
         c.fill();
     }
@@ -45,6 +56,14 @@ function Circle(x, y, dx, dy, radius) {
         this.x += this.dx;
         this.y += this.dy;
 
+        // Interactivity
+        if ((Math.abs(mouse.x - this.x) < 50) && (Math.abs(mouse.y - this.y) < 50)) {
+            this.radius += 1;
+        }
+        else if (this.radius > 2) {
+            this.radius -= 1;
+        }
+
         this.draw();
     }
 }
@@ -53,8 +72,8 @@ function Circle(x, y, dx, dy, radius) {
 // Arc
 var circleArray = [];
 
-for (var i = 0; i < 100; i++) {
-    var radius = 30;
+for (var i = 0; i < 50; i++) {
+    var radius = 50;
     var x = Math.random() * (innerWidth - radius * 2) + radius;
     var y = Math.random() * (innerHeight - radius * 2) + radius;
     var dx = (Math.random() - .5) * 8;
